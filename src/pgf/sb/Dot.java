bc9190aa9a3c494f04e9742313b2b541048c7b46
pdrogfer <pdro74@hotmail.com>
2014-01-27 12:42:24 +0100

    Main layout ok. Drawing on canvas is working. TODO: implement the Dot object. Initialize a collection of 4 (EasyLevel) at random locations of canvas

---
+++ src/pgf/sb/Dot.java
@@ -0,0 +1,13 @@
+/* this is the class for the bubbles, or dots. Implement
+ * set and get position (posX, posY), on click, etc
+ */
+
+package pgf.sb;
+
+public class Dot {
+	
+	// native variables
+	// constructors
+	// methods
+
+}
